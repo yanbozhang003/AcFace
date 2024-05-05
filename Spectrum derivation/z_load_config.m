d5d490cd86af1b731c5fff304d83387770267bf3
YANBO ZHANG <yanbozhang03@gmail.com>
2024-05-05 23:56:06 +0800

    Modify files

--- Spectrum derivation/z_load_config.m
+++
@@ -1,5 +0,0 @@
-function [config,settings] = z_load_config(settings_folder)
-    config = load([settings_folder,'config.mat']).config;
-    settings = load([settings_folder,'settings.mat']).setting;
-end
-
